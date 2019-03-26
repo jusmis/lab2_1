c8fdfa60291762898a4971069bb4d33a402a24aa
Misior.Justyna <195018@edu.p.lodz.pl>
2019-03-26 01:31:04 +0100

    testy_v1 Added test valueIsFirstInMultiElementSequence

--- src/test/java/BinarySearchTests.java
+++ src/test/java/BinarySearchTests.java
@@ -27,4 +27,15 @@ public class BinarySearchTests {
         Assert.assertEquals(EXPECTED_VALUE, result.getPosition());
     }
 
+    @Test
+    public void valueIsFirstInMultiElementSequence() {
+        int seq[] = {1, 2, 3};
+        int value = 1;
+        final int EXPECTED_VALUE = 0;
+
+        SearchResult result = BinarySearch.search(value, seq);
+        Assert.assertTrue(result.isFound());
+        Assert.assertEquals(EXPECTED_VALUE, result.getPosition());
+    }
+
 }
